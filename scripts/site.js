5b44384e1aa3251586ec6897ff536e9c87215c93
calliew927 <Calleaw@byui.edu>
2023-01-19 12:40:22 -0700

    Thursday Class

---
+++ scripts/site.js
@@ -0,0 +1,6 @@
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector('#currentYear').innerHTML = year
+
+
+document.querySelector('#lastModified').textContent = document.lastModified
