ebb1b4be67cc14a7a36b268aaf52915a7ea67325
calliew927 <Calleaw@byui.edu>
2023-01-26 12:31:04 -0700

    Thursday Class

--- lesson3/scripts/site.js
+++ lesson3/scripts/site.js
@@ -1,7 +1,8 @@
 function toggleMenu(){
     document.querySelector('#navbar').classList.toggle('menu-active')
-    document.querySelector('#menu-cloes').classList.toggle('menu-active')
+    document.querySelector('#menu-close').classList.toggle('menu-active')
     document.querySelector('#menu-open').classList.toggle('menu-active')
 }
 
-document.querySelector('#ham-menu').onclick = toggleMenu;
+document.querySelector('#ham-menu').addEventListener('click', toggleMenu);
+
