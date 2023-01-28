93e9979134c13ece1974872e597cbd7d734f139d
calliew927 <Calleaw@byui.edu>
2023-01-28 16:26:19 -0700

    Update CSS

---
+++ chamber/scripts/site.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    document.querySelector('#navbar').classList.toggle('menu-active')
+    document.querySelector('#menu-close').classList.toggle('menu-active')
+    document.querySelector('#menu-open').classList.toggle('menu-active')
+}
+
+document.querySelector('#ham-menu').addEventListener('click', toggleMenu);
