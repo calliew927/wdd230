1177f482517aa64a2d6e5e3bfa68d0b418320b73
calliew927 <Calleaw@byui.edu>
2023-01-31 11:14:53 -0700

    Tuesday Class

--- chamber/scripts/site.js
+++ chamber/scripts/site.js
@@ -5,3 +5,10 @@ function toggleMenu(){
 }
 
 document.querySelector('#ham-menu').addEventListener('click', toggleMenu);
+
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector('#currentYear').innerHTML = year
+
+
+document.querySelector('#lastModified').textContent = document.lastModified
