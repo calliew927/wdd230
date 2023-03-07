77b7d11b4e81d4758dbbbc1df0a0177581288a79
calliew927 <Calleaw@byui.edu>
2023-03-07 08:04:48 -0700

    discover

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,27 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+ 
+const callback = (items, observer) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+};
+
+let options = {
+  threshold: 0.1
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+imagesToLoad.forEach((img) => {
+  observer.observe(img);
+});
