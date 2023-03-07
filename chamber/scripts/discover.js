f78b0357d91b49af64989ad140671ae410e20b53
calliew927 <Calleaw@byui.edu>
2023-03-07 08:39:47 -0700

    fixes

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,3 +1,4 @@
+//lazy loading
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const loadImages = (image) => {
@@ -25,3 +26,35 @@ const observer = new IntersectionObserver(callback, options);
 imagesToLoad.forEach((img) => {
   observer.observe(img);
 });
+
+//days since visited
+date = new Date()
+localStorage.setItem('lastVisit', date);
+
+displayDaysSinceLastVisit();
+
+function displayDaysSinceLastVisit() {
+  const visitsDisplay = document.querySelector('#visit');
+
+  const lastVisit = localStorage.getItem('lastVisit');
+
+  if (!lastVisit) {
+    visitsDisplay.innerText = 'This is your first visit';
+    
+    return;
+  }
+
+  const lastVisitDate = Date.parse(lastVisit);
+  
+  if (!lastVisitDate) {
+    // Stored date is not a valid format
+    return;
+  }
+
+  const currentDate = new Date();
+
+  const difference = currentDate - lastVisitDate;
+  const differenceInDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  visitsDisplay.innerText = differenceInDays;
+}
