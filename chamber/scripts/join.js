da7b09996e73a0dbf82614fa79fb62afd883d77a
calliew927 <Calleaw@byui.edu>
2023-03-11 19:57:18 -0700

    Fixes

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,5 @@
 var today = new Date();
 var dateinput = document.querySelector('#hiddendate');
 dateinput.value = today.toLocaleDateString();
+
+button.onclick = "window.location.href='https://calliew927.github.io/wdd230/chamber/thankyou.html';"
