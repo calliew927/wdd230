90524e0ee1f46fd9c70997468fa7508d1d13f4be
calliew927 <Calleaw@byui.edu>
2023-03-14 11:17:51 -0600

    weatherapi

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,4 +1,3 @@
-// select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
@@ -8,20 +7,15 @@ const lat = '45.9266';
 const lon = '-116.1224';
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=imperial`;
 
-function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${temp}</strong>`;
-
-    const iconsrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+function displayResults(weatherData) {  
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+  const desc = weatherData.weather[0].main;
     
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc
-    speed.textContent = wind
-
-    let desc = data.weather[0].main;
-    let temp = data.main.temp.toFixed();
-    let wind = data.wind.speed;
-    let icon = data.weather[0].icon
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc
+  
 }
 
 async function apiFetch() {
@@ -29,8 +23,10 @@ async function apiFetch() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-      } else {
-          throw Error(await response.text());
+        displayResults(data);
+      } 
+      else {
+        throw Error(await response.text());
       }
     } catch (error) {
         console.log(error);
--- lesson10/weather.js
+++ lesson10/weather.js
@@ -7,20 +7,15 @@ const lat = '45.9266';
 const lon = '-116.1224';
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=imperial`;
 
-function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${temp}</strong>`;
-
-    const iconsrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+function displayResults(weatherData) {  
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+  const desc = weatherData.weather[0].main;
     
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc
-
-
-    let desc = data.weather[0].main;
-    let temp = data.main.temp.toFixed();
-    let wind = data.wind.speed;
-    let icon = data.weather[0].icon
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc
+  
 }
 
 async function apiFetch() {
@@ -28,6 +23,7 @@ async function apiFetch() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
